dfe3f517120aa89efdb129106ddf45e0b14460b3
HOÀNG GIANG LẠI <laihoanggiang.93@gmail.com>
2022-08-07 16:31:49 +0700

    7.8.2022

--- app/js/script.js
+++ app/js/script.js
@@ -1,26 +1,113 @@
+let taskList = [];
+let currentItemIndex = -1;
+
+
 function addTask() {
-    //get user input element
-    let taskNameElement = document.getElementById("task_name")
-    //get value of input element
+    let taskListElem = document.getElementById('task_list');
+    let itemElement = document.createElement('li');
+    itemElement.onclick = onClickItem;
+ 
+    let taskNameElement = document.getElementById("task_name");
+    taskNameElement.required
     let taskName = taskNameElement.value;
-    // create new item element
-    let itemElement = document.createElement('li')
-    // set value for new element
-    itemElement.innerHTML = taskName;
-    //get parent list
-    let taskList = document.getElementById('task_list')
-    //add new item to list
-    taskList.appendChild(itemElement)
+
+    let dateInFuture = document.getElementById("datetime_picker")
+    let dueDate = dateInFuture.value;
+
+    // Add task to task list
+    taskList.push({ taskName, dueDate })
+
+    // Add task to UI
+    itemElement.innerHTML = taskName + " AT " + dueDate;
+    taskListElem.appendChild(itemElement);
+    
+}
+
+
+function onClickItem(event) {
+    let item = event.path[0];
+    let itemParent = item.parentNode;
+    currentItemIndex = Array.prototype.indexOf.call(itemParent.children, item);
+
+    let taskName = document.getElementById("task_name");
+    let dueDate = document.getElementById("datetime_picker");
+    dueDate.value = taskList[currentItemIndex].dueDate;
+    taskName.value = taskList[currentItemIndex].taskName
+
+
+}
+function edit(){
+  
+    let taskName = document.getElementById("task_name").value;
+    let dueDate = document.getElementById("datetime_picker").value;
+
+    taskList[currentItemIndex] = {taskName,dueDate};
+    
+    let taskListElem = document.getElementById('task_list');
+    let itemElement = taskListElem.children
+
+    itemElement[currentItemIndex].innerHTML= taskName + " AT " + dueDate
 
 }
 
-function dueDate(){
-    let dateInFuture = document.getElementsByClassName("due_date")
-    let dueDate = dateInFuture.value;
-    let taskList = document.getElementById('task_list')
-    let dateElement = document.createElement('li');
-    dateElement.innerHTML=dueDate;
-    taskList.appendChild(dateElement)
-    console.log(dueDate)
-    console.log(dateInFuture)
+function del(){
+   taskList.splice(currentItemIndex,1);
+    let taskListElem = document.getElementById('task_list');
+    let itemElement = taskListElem.children;
+    let items = taskListElem.removeChild(itemElement[currentItemIndex])
+    console.log(items)
+    console.log(taskList)
+}
+function compare(a,b){
+    let taskNameA =  a.taskName;
+    let taskNameB = b.taskName;
+    let comparison = 0;
+    if(taskNameA>taskNameB){
+     comparison = 1;
+    }
+    if(taskNameB>taskNameA){
+     comparison = -1;
+    }
+    return comparison;
+ }
+
+function sort(){
+    let taskListSort = taskList.sort(compare)
+    console.log(taskListSort)
+    
+    let taskListElem = document.getElementById('task_list');
+        console.log(taskListElem)
+        let itemElement = taskListElem.children;
+        console.log(itemElement)
+   
+    for(let i=0; i<taskList.length; i++){
+       
+        itemElement[i].innerHTML = taskListSort[i].taskName + " AT  " + taskListSort[i].dueDate
+        console.log(taskListSort[i])
+        
+        }
+        
+   }
+
+function search(){
+    let input = document.getElementById("searchTerm");
+    let taskListElem = document.getElementById('task_list')
+    let listItem = taskListElem.children;
+    // taskList.forEach(function find(task) {
+    //     if(task.taskName.toUpperCase()==input.value.toUpperCase()){
+    //         listItem.style.display = "block";}
+    //     else{
+    //         listItem.style.display = "none";
+    //     }
+    //     }
+    
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].taskName ==input.value){
+            listItem[i].style.display = "block";
+        }
+        else{
+        listItem[i].style.display = "none"};
+        console.log(listItem[i])    
+    }
+   
 }
