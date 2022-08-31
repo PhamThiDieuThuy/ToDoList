e2ae4d199e3a066ea7466d7fc66c59ece333a09b
HOÀNG GIANG LẠI <laihoanggiang.93@gmail.com>
2022-08-31 23:09:15 +0700

    31.08

--- app/js/script.js
+++ app/js/script.js
@@ -1,135 +1,165 @@
 // import {TodoService} from './TodoService';
 
-let taskList = [];
-let currentItemIndex = -1;
 let edittingDiv = null;
+let taskListElem = null;
+let taskNameElement = null;
+let dateInFuture = null;
 
+function onLoad(){
+    taskListElem = document.getElementById('task_list');
+    taskNameElement = document.getElementById("task_name");
+    dateInFuture = document.getElementById("datetime_picker");
+}
+
+//add new Task
 function addTask() {
-    let taskListElem = document.getElementById('task_list');
-    itemElement = document.createElement('li');
-    let taskNameElement = document.getElementById("task_name");
     let taskName = taskNameElement.value;
-    let dateInFuture = document.getElementById("datetime_picker");
     let dueDate = dateInFuture.value;
 
-    taskList.push({ taskName, dueDate })
+    let itemElement = document.createElement('li');
+    taskListElem.appendChild(itemElement);
 
     let selecter = document.createElement('button');
     selecter.setAttribute('id', 'select');
+    itemElement.appendChild(selecter);
     selecter.innerHTML = "..."
+
     selecter.addEventListener('mouseenter', select);
     selecter.addEventListener('mouseleave', leave);
-    itemElement.appendChild(selecter);
-
-    function select() {
-        let ul = document.createElement('ul');
-        ul.setAttribute('id', 'menuEdit')
-        selecter.appendChild(ul);
-        let edit = document.createElement('li');
-        ul.appendChild(edit);
-        edit.innerHTML = "Edit"
-        edit.addEventListener('click', editLi)
-        let del = document.createElement('li');
-        ul.appendChild(del);
-        del.innerHTML = "Delete";
-        del.addEventListener('click', dele)
-        let done = document.createElement('li');
-        ul.appendChild(done);
-        done.addEventListener('click', did);
-        done.innerHTML = "Done"
-
-    }
-    function leave(event) {
-        let btn = event.srcElement
-        btn.removeChild(btn.childNodes[1]);
-
-    }
-
-    taskListElem.appendChild(itemElement);
+     
     let div = document.createElement('div');
     itemElement.appendChild(div);
+
     let h4 = document.createElement('h4')
     h4.innerHTML = dueDate;
-    dateInFuture = document.getElementById("datetime_picker").value = "";
+    dateInFuture.value = "";
     div.appendChild(h4)
+
     let p = document.createElement('p');
     p.innerHTML = taskName;
     document.getElementById("task_name").value = "";
     div.appendChild(p)
     // Save to local storage
-    let newIndex = taskList.length;
+    // let newIndex = taskList.length;
 
-    let key = "todo" + newIndex;
-    let value = taskName + "+" + dueDate;
-    localStorage.setItem(key, value)
+    // let key = "todo" + newIndex;
+    // let value = taskName + "+" + dueDate;
+    // localStorage.setItem(key, value)
 }
 
-function editLi(event) {
+//function mouseenter in list:
+function select(event) {
+    console.log(event)
+    let selecter = event.srcElement;
+   
+    let ul = document.createElement('ul');
+    ul.setAttribute('id', 'menuEdit')
+    selecter.appendChild(ul);
 
+    let edit = document.createElement('li');
+    ul.appendChild(edit);
+    edit.innerHTML = "Edit"
+    edit.addEventListener('click', editLi);
+
+    let del = document.createElement('li');
+    ul.appendChild(del);
+    del.innerHTML = "Delete";
+    del.addEventListener('click', dele);
+
+    let done = document.createElement('li');
+    ul.appendChild(done);
+    done.addEventListener('click', did);
+    done.innerHTML = "Done"
+
+}
+
+//function mouseleave in list:
+function leave(event) {
+    let btn = event.srcElement
+    btn.removeChild(btn.childNodes[1]);
+}
+
+
+//function edit list
+function editLi(event) {
     let editbtn = event.srcElement
     let ul = editbtn.parentNode;
     let btn = ul.parentNode;
+
     edittingDiv = btn.nextElementSibling;
     let h4 = edittingDiv.childNodes;
-    let taskName = document.getElementById("task_name");
-    let dueDate = document.getElementById("datetime_picker");
-    dueDate.value = h4[0].innerHTML;
-    taskName.value = h4[1].innerHTML;
+
+    dateInFuture.value = h4[0].innerHTML;
+    taskNameElement.value = h4[1].innerHTML;
+
     let saveButton = document.getElementById('saveButton');
     saveButton.addEventListener('click', saveEdit);
     document.getElementById('saveButton').style.display = 'initial';
     document.getElementById('add_task').style.display = 'none';
+
     let list = btn.parentNode;
     list.style.backgroundColor = "rgba(112,112,122,0.5";
-
 }
+
+//function save edit
 function saveEdit() {
     if (edittingDiv != null) {
-        console.log(edittingDiv)
-        let h4 = edittingDiv.firstChild
-        console.log(h4)
-        h4.innerHTML = document.getElementById("datetime_picker").value
+        console.log(edittingDiv);
+
+        let h4 = edittingDiv.firstChild;
+        h4.innerHTML = dateInFuture.value;
+
         let p = edittingDiv.lastChild;
-        p.innerHTML = document.getElementById('task_name').value;
-        document.getElementById("datetime_picker").value = '';
-        document.getElementById('task_name').value = '';
+        p.innerHTML = taskNameElement.value;
+
+        dateInFuture.value = '';
+        taskNameElement.value = '';
+
         document.getElementById('saveButton').style.display = 'none';
         document.getElementById('add_task').style.display = 'initial';
+
         let list = edittingDiv.parentNode;
         list.style.backgroundColor = "rgb(225, 248, 237)";
     }
 }
 
+//Function delete list
 function dele(event) {
     let ul = event.path[3];
     ul.remove();
 }
 
+//function to ticked this task has done
 function did(event) {
     console.log(event)
     let btn = event.path[2];
     let div = btn.nextElementSibling;
     let h4 = div.firstChild;
     let p = div.lastChild;
+
     let delh4 = document.createElement('s');
-    h4.appendChild(delh4)
-    let text = h4.firstChild
-    delh4.appendChild(text)
+    h4.appendChild(delh4);
+    let text = h4.firstChild;
+    delh4.appendChild(text);
+
     let delP = document.createElement('s');
     p.appendChild(delP);
-    let text1 = p.firstChild
-    delP.appendChild(text1)
+    let text1 = p.firstChild;
+    delP.appendChild(text1);
 }
 
+// function to sort list
 function sort() {
     let ulOfSort = document.createElement('ul');
     let sortButton = document.getElementById('sort');
     sortButton.appendChild(ulOfSort);
     ulOfSort.setAttribute('id', 'ulOfSort');
-    let sortIncrease = document.createElement('li')
+
+    let sortIncrease = document.createElement('li');
     ulOfSort.appendChild(sortIncrease);
     sortIncrease.innerHTML = "Ascending day"
     sortIncrease.addEventListener('click', ascending);
+
     let sortDecrease = document.createElement('li');
     ulOfSort.appendChild(sortDecrease);
     sortDecrease.innerHTML = "Descending day";
@@ -214,7 +244,7 @@ function leaveSort() {
     }
 }
 
-
+//function to search
 
 function search() {
     let searchItem = document.getElementById("searchTerm");
